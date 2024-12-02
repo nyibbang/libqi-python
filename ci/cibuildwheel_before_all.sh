060b183061a0f5b3d9d90098ce7c45946bdb1a5a
Vincent Palancher <vincent.palancher@aldebaran.com>
2024-12-02 12:46:53 +0100

    Update CI before_all script for any OS

--- ci/cibuildwheel_before_all.sh
+++ ci/cibuildwheel_before_all.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 set -x -e
 
-PACKAGE=$1
+PACKAGE_DIR=$1
+DEPS_DIR="$PACKAGE_DIR/build/deps"
+mkdir -p "$DEPS_DIR"
 
 pip install 'conan>=2'
 
-# Perl dependencies required to build OpenSSL.
-yum install -y perl-IPC-Cmd perl-Digest-SHA
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Perl dependencies required to build OpenSSL.
+    yum install -y perl-IPC-Cmd perl-Digest-SHA
+fi
 
 # Install Conan configuration.
 conan profile detect
-conan config install "$PACKAGE/ci/conan"
-
-mkdir -p /work
+conan config install "$PACKAGE_DIR/ci/conan"
 
 # Clone and export libqi to Conan cache.
 git clone \
     --branch master \
     https://github.com/aldebaran/libqi.git \
-    /work/libqi
-conan export /work/libqi
+    "$DEPS_DIR/libqi-git"
+conan export "$DEPS_DIR/libqi-git"
 
 # Install dependencies of libqi-python from Conan, including libqi.
 #
@@ -32,4 +34,4 @@ conan install "$PACKAGE" \
     --profile:all default \
     --profile:all cppstd17
     --options cibuildwheel=True
-    --output-folder /work/conan
+    --output-folder "$DEPS_DIR"
